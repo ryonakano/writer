c653572860b4e99c82a6cec8d9de35e716154553
Tuur Dutoit <me@tuurdutoit.be>
2014-08-16 19:00:01 +0200

    Updated build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,7 +8,11 @@ echo
 
 valac --pkg gtk+-3.0 --pkg granite -o writer src/config.vala src/Writer.vala src/MainWindow.vala src/Widgets/TitleBar.vala src/Widgets/ToolBar.vala src/Widgets/EditorView.vala src/Widgets/WelcomeView.vala src/Utils/Editor.vala src/Utils/FileChooser.vala src/Utils/File.vala
 
+RESULT=$?
+
 echo
 echo "DONE Building"
 echo "===="
 echo
+
+exit $RESULT
